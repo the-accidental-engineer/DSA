185e25c31cf4d10cf127b2740306fa03f74ade86
Prateek Srivastav <89292014+the-accidental-engineer@users.noreply.github.com>
2024-05-25 23:27:10 +0530

    Create Missing_Number.java

---
+++ Missing_Number.java
@@ -0,0 +1,10 @@
+class Missing_Number {
+    public int missingNumber(int[] nums) {
+        int size = nums.length;
+        int sum = (size * (size + 1)) / 2;
+        for (int val : nums) {
+            sum -= val;
+        }
+        return sum;
+    }
+}
