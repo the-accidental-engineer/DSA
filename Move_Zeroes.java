728e2fc77a90fbb6fe80d6e93fdd35f4b4d04d86
Prateek Srivastav <89292014+the-accidental-engineer@users.noreply.github.com>
2024-05-27 23:24:06 +0530

    Update Move_Zeroes.java

--- Move_Zeroes.java
+++ Move_Zeroes.java
@@ -35,14 +35,14 @@ public class Move_Zeroes {
     }
 
 static void moveZeroes(int[] nums) {
-        int size = nums.length;
-        int k=0;
-        int [] tempArr = new int [size];
-        for(int i =0; i<size; i++){
-            if(nums[i]!=0){
-                tempArr[k]=nums[i];
-                k++;
+         int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[idx++] = nums[i];
             }
         }
-        System.arraycopy(tempArr, 0, nums, 0, tempArr.length);
+        for (int i = idx; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        return;
 }
