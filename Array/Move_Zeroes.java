6b3997ab0e2aa49e94fad874d2def43904421e3a
Prateek Srivastav <prateek.srivastav.7374@gmail.com>
2025-01-02 23:55:51 +0530

    updated move zero

--- Array/Move_Zeroes.java
+++ Array/Move_Zeroes.java
@@ -35,14 +35,15 @@ public class Move_Zeroes {
     }
 
 static void moveZeroes(int[] nums) {
-         int idx = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[idx++] = nums[i];
+    if (nums.length > 1) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
         }
-        for (int i = idx; i < nums.length; i++) {
-            nums[i] = 0;
-        }
-        return;
+    }
 }
