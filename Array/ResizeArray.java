b15e145ab7b8f696a2886e030b6b58246ba0cc25
the-accidental-engineer <prateek.srivastav.1212@gmail.com>
2025-01-06 03:46:45 +0530

    ResizeArray

---
+++ Array/ResizeArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ResizeArray {
+    public static void main(String[] args) {
+        ResizeArray resizeArray = new ResizeArray();
+        int [] input = new int[]{1,2,3,4,5,6,7,8,9,10};
+        int [] result = resizeArray.ResizeArrayFunction(input,5);
+        System.out.println("Input :" + Arrays.toString(input) + " Size : " + input.length);
+        System.out.println("Input :" + Arrays.toString(result) + " Size : " + result.length);
+    }
+
+    public int [] ResizeArrayFunction(int[] arr, int newCapacity){
+        int [] temp = new int[newCapacity];
+        for(int i=0; i<newCapacity; i++){
+            temp[i] = arr[i];
+        }
+        return temp;
+    }
+}
